fffa88cc5c65fd2d76a6382bb780db6be73e5b0a
Ajao Temitope <ajaotemitope5@gmail.com>
2026-06-08 23:12:44 +0100

    Fix liaison officer cache with timestamp busting

--- frontend/src/pages/MentorDashboard.tsx
+++ frontend/src/pages/MentorDashboard.tsx
@@ -514,10 +514,7 @@ function LiaisonOfficersTab({ onOfficersChange }: { onOfficersChange: (officers:
 
   const load = async () => {
     try {
-      const res = await api.get('/mentor/liaison-officers', {
-        headers: { 'Cache-Control': 'no-cache' }
-      }
-      )
+      const res = await api.get('/mentor/liaison-officers?t=${Date.now()}')
       setOfficers(res.data)
       onOfficersChange(res.data)
     } catch {}
